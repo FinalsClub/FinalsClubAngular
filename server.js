6929586fc1088a075b853d50ef174827459bf98a
Kara Erickson <kara.erickson3@gmail.com>
2014-02-14 14:34:07 -0800

    Altered database to take advantage of Mongo

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 
 var http = require('http');
+var url = require('url');
 
 var port = 8080;
 var ip = '127.0.0.1';
 
-//creates server on port 8080
+//creates server on local port 8080
 http.createServer(requestHandler).listen(port, ip);
 
 //sends response back to client
@@ -16,12 +17,14 @@ var sendResponse = function(status, headers, obj, resp) {
 //sets up request method control structure
 var reqMethods = {
   'GET': function(req, res) {
+    //control flow here to replace'null' with res obj depending on req.url (parse with url module)    
     sendResponse(200, defaultHeaders, null, res); 
   },
   'OPTIONS': function(req, res) {
     sendResponse(200, defaultHeaders, null, res); 
   },
   'POST': function(req, res) {
+    //process req data with callbacks here
     sendResponse(201, defaultHeaders, null, res); 
   },
   'PUT': function(req, res) {
