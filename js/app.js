b19f35b8bf6a0a3db9403290d3061177e5dff9b6
lennylemony <anastasia.aizman@gmail.com>
2014-02-15 10:34:40 -0800

    added routes

--- js/app.js
+++ js/app.js
@@ -1,16 +1,40 @@
 app = angular.module('app', ['ngRoute']);
-app.config(function ($routeProvider) {
-    $routeProvider.when('/', {
-      // controller: '',
-      templateUrl: 'index.html'
-    }).when('/myGroups', {
-      templateUrl: 'mygroups.jade' // or .html?
-    }).otherwise({
-      redirectTo: '/'
-    });
-  });
+app.config(function ($httpProvider, $routeProvider, $locationProvider) {
+  $httpProvider.responseInterceptors.push(interceptor);
+  $locationProvider.html5Mode(true);
+  $routeProvider
+    .when('/', { template: 'index.jade', controller: 'UserController' })
+    .when('/login', { template: 'login.jade', controller: 'LogInController' })
+    .when('/groups/'+group.id+'/flashcards', { template: 'flashcards.jade', controller: '' })
+    .when('/groups/'+group.id+'/flashcards'+lecture.id+'', { template: 'lectureFlashcards.jade', controller: '' })
+    .when('/groups/'+group.id+'/flashcards'+lecture.id+'/edit', { template: 'editFlashcards.jade', controller: '' })
+    .when('/groups/search', { template: 'searchGroups.jade', controller: '' })
+    .when('/groups/new', { template: 'createGroups.jade', controller: '' })
+    .when('groups/'+group.id+'/communications', { template: 'communications.jade', controller: '' })
+    .otherwise({ redirectTo: '/' });
+});
 
-////////////////////// USER-CENTRIC CONTROLLERS
+// app.run($http, $rootscope){
+//   $http.get('/')
+//     .success(function(data, status, headers, config){
+//       $rootscope.user = data; // user object
+//     })
+//     .error()
+// }
+    /////// http interceptor for USER object: 
+    ///// user obj comes with group names
+    ///// more get requests for lectures, etc
+app.controller('LogInController', function($scope){
+
+})
+
+app.controller('UserController', function($rootscope, $scope) {
+  // if user exists
+  $rootscope.groups = user.groups;
+});
+
+
+
 
 //// mygroups controller
 app.controller('allGroupsViewController', 'getUsersGroups', function($scope){
@@ -21,7 +45,7 @@ app.controller('allGroupsViewController', 'getUsersGroups', function($scope){
 //// get User's groups
 app.factory('getUsersGroups', function(){
   return {
-    getGroups: function(option){
+    getGroups: function(){
       $http({
         method : 'GET',
         url : '/getGroupsURL',
@@ -35,7 +59,23 @@ app.factory('getUsersGroups', function(){
   }
 });
 
-////////////////////// GROUP-CENTRIC CONTROLLERS
+
+// app.factory('getUsersGroups', function(){
+//   return {
+//     getGroups: function(option){
+//       $http({
+//         method : 'GET',
+//         url : '/getGroupsURL',
+//         params: { user_id: user.id }
+//         }).success(function(data, status, headers, config) {
+//           return data; // should be an array
+//         }).error(function(data, status, headers, config) {
+//           console.log(status, error)
+//       });    
+//     }
+//   }
+// });
+
 
 app.controller('groupController', 'getGroupsLectures', function($scope){
   $scope.groupSubject = getGroupsLectures.getSubject;
@@ -67,4 +107,8 @@ app.factory('getGroupsLectures', function(){
       });    
     }
   }
+})
+
+app.controller('createGroupController', function($scope){
+
 })
