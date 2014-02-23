49260e2d37edd732847259cb4661dfaa888ee26e
Kara Erickson <kara.erickson3@gmail.com>
2014-02-22 21:55:29 -0800

    Reordered redis config

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 
 //require dependencies
+if (process.env.REDISTOGO_URL) { 
+  var rtg = require('url').parse(process.env.REDISTOGO_URL);
+  config.redis.port = rtg.port;
+  config.redis.host = rtg.hostname;
+  config.redis.password = rtg.auth.split(":")[1];  
+}
 
 var express = require('express');
 var models = require('./models');
@@ -7,9 +13,8 @@ var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
 var auth = require('./authentication.js');
 var sharejs = require('share').server;
-var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+
 var redis = require("redis").createClient(rtg.port, rtg.hostname);
-redis.auth(rtg.auth.split(":")[1]);
 var RedisStore = require('connect-redis')(express);
 
 //set up server
