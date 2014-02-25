38a65ed87f17342cdbcb19e9ac9b5349305561b6
Kara Erickson <kara.erickson3@gmail.com>
2014-02-24 20:13:30 -0800

    Changed size of text based on length

--- public/js/app.js
+++ public/js/app.js
@@ -156,6 +156,25 @@ app.controller('flashcardController', ['$scope', '$timeout', function($scope, $t
   $scope.flipped = false;
   $scope.currentCard = 0;
   
+  $scope.configureText = function(counter, side) {
+    if (counter === $scope.flashcards.length) {
+      return;
+    }    
+    var el = angular.element('#div-' + counter + " " + side + ' p');
+    
+    if (el.data('length') < 60) {
+      el.addClass('big');
+    } else if (el.data('length') < 200) {
+      el.addClass('medium');
+    } else {
+      el.addClass('small');
+    }
+      
+    side = side === ".back" ? ".front" : ".back";
+    counter = side === ".front" ? counter + 1 : counter;
+    $scope.configureText(counter, side);
+  };
+  
   $scope.goBack = function() {
     if ($scope.currentCard > 0) {
       $scope.currentCard--;
@@ -165,12 +184,18 @@ app.controller('flashcardController', ['$scope', '$timeout', function($scope, $t
   $scope.goForward = function() {
     if ($scope.currentCard < $scope.flashcards.length-1) {
       $scope.currentCard++;
-    }
+    }        
   };
   
   $scope.flip = function(index) {    
     angular.element('#div-' + index).toggleClass('animated');
   };
+  
+  angular.element(document).ready(function() {
+    $scope.configureText(0, ".front");    
+  });
+
+  
 }]);
 
 app.controller('shareController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
