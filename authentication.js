5b46edfc1a6942f35288530796bb1ed3d8d5ef20
Kara Erickson <kara.erickson3@gmail.com>
2014-02-24 19:20:00 -0800

    Commented out config

--- authentication.js
+++ authentication.js
@@ -1,7 +1,7 @@
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
 var User = require('./models').User;
-var config = require('./oauth.js')
+// var config = require('./oauth.js')
 
 module.exports = passport.use(new FacebookStrategy(
 
