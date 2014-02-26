e7fe9dc195f02232fbd28b2acd27bf4fea702608
Kara Erickson <kara.erickson3@gmail.com>
2014-02-26 11:36:24 -0800

    Removed redis pending changes

--- public/js/groupsView.js
+++ public/js/groupsView.js
@@ -1,17 +1,7 @@
 app.controller('allGroupsViewController', [ '$scope', '$http', function($scope, $http){
   $scope.groups = $scope.groups || [];
   $scope.currentGroup = null;
-  $scope.sendLeaveGroupData = function(id){
-    $http({
-      method: 'GET', 
-      url: '/leave_group',
-      data: JSON.stringify(id)
-    }).success(function(data, status){
-    }).error(function(data){
-      console.log('error deleting: ', data)
-    });
-  };
-
+  
   $scope.leaveGroup = function(){
     $scope.group_id = window.location.pathname.split('/')[2];
     $http({
