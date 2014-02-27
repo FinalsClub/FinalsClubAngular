1dc383b4b60f756b08ff23829edefb827943b320
Kara Erickson <kara.erickson3@gmail.com>
2014-02-26 16:06:19 -0800

    Fixed module.exports variable error

--- redis.js
+++ redis.js
@@ -3,8 +3,8 @@ var redis;
 
 if (process.env.REDISTOGO_URL) {
   module.exports.rtg = require('url').parse(process.env.REDISTOGO_URL);
-  redis = require('redis').createClient(rtg.port, rtg.hostname);
-  redis.auth(rtg.auth.split(':')[1]);  
+  redis = require('redis').createClient(module.exports.rtg.port, module.exports.rtg.hostname);
+  redis.auth(module.exports.rtg.auth.split(':')[1]);  
 } else {
   module.exports.rtg = {
     hostname: null,
